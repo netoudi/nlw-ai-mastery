805f212308e2dd1208b07bf89f86bcf963955373
netoudi <netoudi@rocketmail.com>
2023-09-12 22:51:00 -0300

    chore(web): config prettier-plugin-tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 
 export function App() {
   return (
-    <div className="flex justify-between items-center p-4">
+    <div className="flex items-center justify-between p-4">
       <h1>upload.ia</h1>
       <Button>Github</Button>
     </div>
